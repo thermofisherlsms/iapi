a200f4267c8894379406128be66f9f5b32fa8baa
Derek Bailey <dbaileychess@gmail.com>
2016-04-25 13:25:27 -0700

    Added compiled libs and example client for Fusion

---
+++ examples/fusion-example-client/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace FusionExampleClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.serverConnectedTB = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.instrumentConnectedTB = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.instrumentIdTB = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.instrumentNameTB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.systemStateTB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.systemModeTB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button7 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start Online Access";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 214);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Dispose";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 75);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "GetInstrumentAccess";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 128);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "GetScanContainer";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(337, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(174, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Number of Scans Arrived";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(322, 185);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "OffMode";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(322, 214);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "OnMode";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(511, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(553, 244);
+            this.tabControl1.TabIndex = 9;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.serverConnectedTB);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.instrumentConnectedTB);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.instrumentIdTB);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.instrumentNameTB);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.systemStateTB);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.systemModeTB);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(545, 218);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "ReadBacks";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // serverConnectedTB
+            // 
+            this.serverConnectedTB.Location = new System.Drawing.Point(110, 7);
+            this.serverConnectedTB.Name = "serverConnectedTB";
+            this.serverConnectedTB.Size = new System.Drawing.Size(100, 20);
+            this.serverConnectedTB.TabIndex = 21;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 10);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(93, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Server Connected";
+            // 
+            // instrumentConnectedTB
+            // 
+            this.instrumentConnectedTB.Location = new System.Drawing.Point(427, 63);
+            this.instrumentConnectedTB.Name = "instrumentConnectedTB";
+            this.instrumentConnectedTB.Size = new System.Drawing.Size(100, 20);
+            this.instrumentConnectedTB.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(296, 66);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(111, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Instrument Connected";
+            // 
+            // instrumentIdTB
+            // 
+            this.instrumentIdTB.Location = new System.Drawing.Point(427, 36);
+            this.instrumentIdTB.Name = "instrumentIdTB";
+            this.instrumentIdTB.Size = new System.Drawing.Size(100, 20);
+            this.instrumentIdTB.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(337, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Instrument ID";
+            // 
+            // instrumentNameTB
+            // 
+            this.instrumentNameTB.Location = new System.Drawing.Point(427, 10);
+            this.instrumentNameTB.Name = "instrumentNameTB";
+            this.instrumentNameTB.Size = new System.Drawing.Size(100, 20);
+            this.instrumentNameTB.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(338, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Instrument Name";
+            // 
+            // systemStateTB
+            // 
+            this.systemStateTB.Location = new System.Drawing.Point(110, 62);
+            this.systemStateTB.Name = "systemStateTB";
+            this.systemStateTB.Size = new System.Drawing.Size(100, 20);
+            this.systemStateTB.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "System State";
+            // 
+            // systemModeTB
+            // 
+            this.systemModeTB.Location = new System.Drawing.Point(110, 36);
+            this.systemModeTB.Name = "systemModeTB";
+            this.systemModeTB.Size = new System.Drawing.Size(100, 20);
+            this.systemModeTB.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "System Mode";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(545, 218);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(159, 75);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(130, 23);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "SendScan";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1086, 281);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox systemModeTB;
+        private System.Windows.Forms.TextBox systemStateTB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox instrumentIdTB;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox instrumentNameTB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox instrumentConnectedTB;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox serverConnectedTB;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ examples/fusion-example-client/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Thermo.TNG.Factory;
+using Thermo.Interfaces.InstrumentAccess_V1;
+using Thermo.Interfaces.InstrumentAccess_V1.MsScanContainer;
+using Thermo.Interfaces.FusionAccess_V1;
+using Thermo.Interfaces.FusionAccess_V1.MsScanContainer;
+using Thermo.Interfaces.InstrumentAccess_V1.Control;
+using Thermo.Interfaces.InstrumentAccess_V1.Control.Acquisition.Modes;
+using Thermo.Interfaces.InstrumentAccess_V1.Control.Acquisition;
+using Thermo.Interfaces.InstrumentAccess_V1.Control.InstrumentValues;
+using Thermo.Interfaces.InstrumentAccess_V1.Control.Scans;
+
+namespace FusionExampleClient
+{
+    public partial class Form1 : Form
+    {
+        IFusionInstrumentAccessContainer _instAccessContainer;
+        IFusionInstrumentAccess _instAccess;
+        IFusionMsScanContainer _instMSScanContainer;
+        IAcquisition _instAcq;
+        IControl _instControl;
+        IInstrumentValues _instValues;
+        IScans _scans;
+
+        int totalScansArrived = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _instAccessContainer = Factory<IFusionInstrumentAccessContainer>.Create();
+            _instAccessContainer.ServiceConnectionChanged += _instAccessContainer_ServiceConnectionChanged;
+            _instAccessContainer.MessagesArrived += _instAccessContainer_MessagesArrived;
+        }
+
+        void _instAccessContainer_MessagesArrived(object sender, MessagesArrivedEventArgs e)
+        {
+
+        }
+
+        void _instAccessContainer_ServiceConnectionChanged(object sender, EventArgs e)
+        {
+            Invoke(new Action(
+            () =>
+            {
+                serverConnectedTB.Text = _instAccessContainer.ServiceConnected.ToString();
+            }));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _instAccessContainer.StartOnlineAccess();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _instAccessContainer.Dispose();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            _instAccess = _instAccessContainer.Get(1);
+
+            instrumentIdTB.Text = _instAccess.InstrumentId.ToString();
+            instrumentNameTB.Text = _instAccess.InstrumentName;
+            instrumentConnectedTB.Text = _instAccess.Connected.ToString();
+
+            _instControl = _instAccess.Control;
+            _instAccess.ConnectionChanged += _instAccess_ConnectionChanged;
+            _instAcq = _instControl.Acquisition;
+            _instAcq.StateChanged += Acquisition_StateChanged;
+            UpdateState(_instAcq.State);
+
+
+            _instValues = _instControl.InstrumentValues;
+            _scans = _instControl.GetScans(false);
+
+        }
+
+        void Acquisition_StateChanged(object sender, StateChangedEventArgs e)
+        {
+            UpdateState(e.State);
+        }
+
+        private void UpdateState(IState state)
+        {
+            if (state != null)
+            {
+                Invoke(new Action(
+                () =>
+                {
+                    systemModeTB.Text = Enum.GetName(typeof(SystemMode), state.SystemMode);
+                    systemStateTB.Text = Enum.GetName(typeof(InstrumentState), state.SystemState);
+                }));
+            }
+        }
+
+
+        void _instAccess_ConnectionChanged(object sender, EventArgs e)
+        {
+            Invoke(new Action(
+            () =>
+            {
+                instrumentConnectedTB.Text = _instAccess.Connected.ToString();
+            }));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            _instMSScanContainer = _instAccess.GetMsScanContainer(0);
+            _instMSScanContainer.MsScanArrived += _instMSScanContainer_MsScanArrived;
+            _instMSScanContainer.AcquisitionStreamOpening += _instMSScanContainer_AcquisitionStreamOpening;
+            _instMSScanContainer.AcquisitionStreamClosing += _instMSScanContainer_AcquisitionStreamClosing;
+        }
+
+        void _instMSScanContainer_AcquisitionStreamOpening(object sender, MsAcquisitionOpeningEventArgs e)
+        {
+
+        }
+
+        void _instMSScanContainer_AcquisitionStreamClosing(object sender, EventArgs e)
+        {
+
+        }
+
+        void _instMSScanContainer_MsScanArrived(object sender, MsScanEventArgs e)
+        {
+            var lastScan1 = _instMSScanContainer.GetLastMsScan();
+            var lastScan2 = e.GetScan();
+            if (lastScan1 == lastScan2)
+                totalScansArrived++;
+            Invoke(new Action(
+            () =>
+            {
+                textBox1.Text = totalScansArrived.ToString();
+            }));
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            IMode mode = _instControl.Acquisition.CreateOffMode();
+            _instControl.Acquisition.SetMode(mode);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            IMode mode = _instControl.Acquisition.CreateOnMode();
+            _instControl.Acquisition.SetMode(mode);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            var possibleParameters = _scans.PossibleParameters;
+
+            ICustomScan customScan = _scans.CreateCustomScan();
+            customScan.Values["FirstMass"] = "252";
+            customScan.Values["LastMass"] = "1985";
+            customScan.Values["ScanType"] = "SIM";
+            customScan.Values["MicroScans"] = "2";
+
+            _scans.SetCustomScan(customScan);
+        }
+
+
+    }
+}
