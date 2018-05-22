bcd758924a874185f89523e59633df23285bbc19
Thermo Scientific <Thermo Scientific@THERMO-JJ312FUU>
2018-05-22 15:00:42 -0700

    Updated Minified example for Fusion

--- examples/tribrid/MinifiedExample/Program.cs
+++ examples/tribrid/MinifiedExample/Program.cs
@@ -12,22 +12,45 @@ namespace MinifiedExample
         {
             // Use the Factory creation method to create a Fusion Access Container
             IFusionInstrumentAccessContainer fusionContainer = Factory<IFusionInstrumentAccessContainer>.Create();
-                    
-            // From the instrument container, get access to a particular instrument
-            IFusionInstrumentAccess fusionAccess = fusionContainer.Get(1);
 
+            // Connect to the service by going 'online'
             fusionContainer.StartOnlineAccess();
 
+            // Wait until the service is connected 
+            // (better through the event, but this is nice and simple)
+            while (!fusionContainer.ServiceConnected);
+
+            // From the instrument container, get access to a particular instrument
+            IFusionInstrumentAccess fusionAccess = fusionContainer.Get(1);
+                 
+            // Get the MS Scan Container from the fusion
             IFusionMsScanContainer fusionScanContainer = fusionAccess.GetMsScanContainer(0);
 
-            fusionScanContainer.MsScanArrived += FusionScanContainer_MsScanArrived;     
-
-            while (Console.ReadKey().Key != ConsoleKey.Escape);        
+            // Run forever until the user Escapes
+            ConsoleKeyInfo cki;
+            while ((cki = Console.ReadKey()).Key != ConsoleKey.Escape)
+            {
+                switch(cki.Key)
+                {             
+                    case ConsoleKey.S:
+                        // Subscribe to whenever a new MS scan arrives
+                        fusionScanContainer.MsScanArrived += FusionScanContainer_MsScanArrived;
+                        break;
+                    case ConsoleKey.U:
+                        // Unsubscribe 
+                        fusionScanContainer.MsScanArrived -= FusionScanContainer_MsScanArrived;
+                        break;
+                    default:
+                        Console.WriteLine("Unsupported Key: {0}", cki.Key);
+                        break;
+                }
+            }
         }
 
         private static void FusionScanContainer_MsScanArrived(object sender, MsScanEventArgs e)
         {
-            Console.WriteLine("Received MS Scan: "+ e.GetScan().Header["ScanNumber"]);
+            // Print out the scan number of the scan received to console
+            Console.WriteLine("Received MS Scan Number: {0}", e.GetScan().Header["Scan"]);
         }
     }
 }
