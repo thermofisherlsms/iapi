ae8b6ab9233945430c63f9815a7739fe9fed47e3
Kfort01 <87116944+Kfort01@users.noreply.github.com>
2021-08-02 13:31:11 +0200

    Add files via upload

---
+++ examples/Exploris/1 InitialConnect/Connect.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using Microsoft.Win32;
+
+using Thermo.Interfaces.ExplorisAccess_V1;
+
+namespace InitialConnect
+{
+	class Connect
+	{
+		const string DefaultBasePath = "Thermo\\Exploris";			// Default path of the instrument's config file.
+		const string DefaultRegistry = "SOFTWARE\\Thermo Exploris";	// Name of the instrument's host in the registry.
+		const string XmlRoot = "DataSystem";						// base element in the config file
+		const string ApiFileNameDescriptor = "ApiFileName";			// Registry connect string of the assembly
+		const string ApiClassNameDescriptor = "ApiClassName";       // Registry connect string of the implementing class
+
+
+		/// <summary>
+		/// Create an instance of the API object and return it. Exceptions will be thrown on errors.
+        /// Please also check "2 KeepInstrumentConnection\Connection.cs" for a version of generic use.
+		/// </summary>
+		/// <returns>Returns a new instance of the API object representing an Exploris series instrument.</returns>
+		static internal IExplorisInstrumentAccessContainer GetApiInstance()
+		{
+			string basePath = null;
+			// Check the registry first. There should be an entry where programs (and also data) is located.
+			using (RegistryKey key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+			{
+				using (RegistryKey merkur = key.OpenSubKey(DefaultRegistry))
+				{
+					if (merkur != null)
+					{
+						basePath = (string) merkur.GetValue("data", null /* in case that only the key is present */);
+					}
+				}
+			}
+
+			if ((basePath == null) || !File.Exists(Path.Combine(basePath, XmlRoot + ".xml")))
+			{
+				// This condition can be treated already as an error. But is shall be demonstrated on how to find API otherwise.
+				// Note that the preferred way is using the registry.
+				basePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), DefaultBasePath);
+			}
+
+			XmlDocument doc = new XmlDocument();
+			doc.Load(Path.Combine(basePath, XmlRoot + ".xml"));
+			string filename = doc[XmlRoot][ApiFileNameDescriptor].InnerText.Trim();
+			string classname = doc[XmlRoot][ApiClassNameDescriptor].InnerText.Trim();
+
+			if (!File.Exists(filename))
+			{
+				// assume GAC specification, this is the last chance we have
+				return (IExplorisInstrumentAccessContainer) Assembly.Load(filename).CreateInstance(classname);
+			}
+			return (IExplorisInstrumentAccessContainer) Assembly.LoadFrom(filename).CreateInstance(classname);
+		}
+	}
+}
---
+++ examples/Exploris/1 InitialConnect/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Thermo.Interfaces.ExplorisAccess_V1;
+
+namespace InitialConnect
+{
+	class Program
+	{
+		/// <summary>
+		/// This program is a console mode program running under .NET 4.6 and maybe even older.
+		/// <para>
+		/// It demonstrates how to access an API instance for a Exploris style instrument.
+		/// </para>
+		/// </summary>
+		/// <param name="args">ignored</param>
+		static void Main(string[] args)
+		{
+			// In opposite to Exactive, the container is disposable, not the instrument
+			using (IExplorisInstrumentAccessContainer container = Connect.GetApiInstance())
+			{
+				IExplorisInstrumentAccess instrument = container.Get(1);
+				// we just print the instrument name to proof success
+				Console.WriteLine(instrument.InstrumentName);
+			}
+
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadKey();
+		}
+	}
+}
