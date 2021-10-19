0de4ea7525e2b17bbab8f90ca27819d217a29c32
Canterbury, Jesse <jesse.canterbury@thermofisher.com>
2021-10-19 10:59:09 -0700

    Update patch to 20210901; add IAPIFusionCustomScan class

---
+++ examples/tribrid/fusion-example-client/IAPIFusionCustomScan.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thermo.Interfaces.FusionAccess_V1.Control.Scans;
+
+namespace FusionExampleClient
+{
+    //
+    // This class provides a wrapper for the FusionCustomScan, allowing access to the FusionCustomScan properties and methods.
+    //
+    public class IAPIFusionCustomScan : IFusionCustomScan
+    {
+        public IAPIFusionCustomScan()
+        {
+            Values = new Dictionary<string, string>();
+        }
+
+        public bool IsPAGCScan { get; set; }
+        public long PAGCGroupIndex { get; set; }
+        public double SingleProcessingDelay { get; set; }
+        public IDictionary<string, string> Values { get; set; }
+        public long RunningNumber { get; set; }
+    }
+}
